6a700048833d348a4b9028cc046c776041af4638
Carl Parm <carlparm@gmail.com>
2020-01-22 20:36:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -41,12 +41,14 @@ def movies_with_director_key(name, movies_collection)
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   index = 0
+  new_array =[]
   while movies < movies_collection.length do
-  updated_movie = movie_with_director_name(name ,movies_collection[index])
+    updated_movie = movie_with_director_name(name, movies_collection[index])
+    new_array.push(updated_movie)
+    index += 1
+  end
 
-  
-  
-  
+return new_array
   # and accumulate the returned Array of movies into a new Array that's
   # returned by this method.
   #
