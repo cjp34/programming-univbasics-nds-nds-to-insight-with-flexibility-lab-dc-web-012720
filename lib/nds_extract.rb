c199b88f14f849ff2740865833089574659994b3
Carl Parm <carlparm@gmail.com>
2020-01-22 20:36:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -42,7 +42,7 @@ def movies_with_director_key(name, movies_collection)
   # of movies and a directors name to the movie_with_director_name method
   index = 0
   new_array =[]
-  while movies < movies_collection.length do
+  while index < movies_collection.length do
     updated_movie = movie_with_director_name(name, movies_collection[index])
     new_array.push(updated_movie)
     index += 1
