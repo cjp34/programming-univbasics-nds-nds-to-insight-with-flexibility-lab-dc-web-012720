017a66b8b06052e1f1a092fe72adbe0aec1b484d
Carl Parm <carlparm@gmail.com>
2020-01-22 20:56:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -64,10 +64,14 @@ end
 
 
 def gross_per_studio(collection)
-  test_data = collection
-  binding.pry
   
-  ind
+  result = {}
+  index = 0
+  while index < collection.length do
+    studio_name = collection[index][:studio]
+    studio_gross = collection[index][:worldwide_gross]
+    
+    if !result
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
