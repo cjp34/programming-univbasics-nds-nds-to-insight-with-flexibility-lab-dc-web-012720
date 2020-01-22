c4a6b6a4e8ed0f5a987e02127a46f118432122a0
Carl Parm <carlparm@gmail.com>
2020-01-22 21:07:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -95,6 +95,8 @@ return result
 end
 
 def movies_with_directors_set(source)
+  test_data = source
+  
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
